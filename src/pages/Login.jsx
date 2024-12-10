a7534f0ea54b287c9f1cd6c8d39fac1d7c3c642a
sreeja o s <sreejaosreenivasan@gmail.com>
2024-12-10 13:35:49 +0530

    deploy2

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -18,7 +18,7 @@ export default function Login() {
     setError(null);
   
     try {
-      const response = await fetch('http://localhost:5000/user/login', {
+      const response = await fetch('https://backend-gofood-aq0x.onrender.com/user/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(inputs),
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -16,7 +16,7 @@ function Signup() {
     setError(null);
 
     try {
-      const response = await fetch("http://localhost:5000/user/register", {
+      const response = await fetch("https://backend-gofood-aq0x.onrender.com/user/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(inputs),
