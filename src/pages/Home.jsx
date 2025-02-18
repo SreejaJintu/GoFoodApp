ff347199edac8fb5b53ef4414bf9f0d97eaf1867
sreeja o s <sreejaosreenivasan@gmail.com>
2025-02-18 22:36:34 +0530

    change url

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -133,7 +133,7 @@ function Home() {
 
   const loadData = useCallback(async () => {
     try {
-      const response = await fetch("http://localhost:5000/food/display", {
+      const response = await fetch("https://backend-gofood-aq0x.onrender.com/food/display", {
         method: "GET",
         headers: { "Content-Type": "application/json" },
       });
